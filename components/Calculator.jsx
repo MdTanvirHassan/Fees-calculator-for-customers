f43e8376aa1ad7e397cfbe42928c982054227d3a
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-03-08 23:43:48 +0600

    Update Calculator.jsx

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Calculator = () => {
   const [amount, setAmount] = useState(0);
-  const [interest, setInterest] = useState(0);
+  const [interest, setInterest] = useState();
   const [time, setTime] = useState(0);
   const [emi, setEmi] = useState(0);
   const [totalinterest, setTotalinterest] = useState(0);
@@ -19,21 +21,21 @@ const Calculator = () => {
   };
 
   const calculateLoan = () => {
-    if (principal > 0 && interest > 0 && time > 0) {
+    if (principal > 0 && interest > 0 ) {
       let p = parseFloat(principal);
       let r = parseFloat(interest);
-      let n = parseFloat(time);
+      //let n = parseFloat(time);
 
       let actualRate = parseFloat(r / 12 / 100);
 
       let calcemi =
         p *
         actualRate *
-        (Math.pow(1 + actualRate, n) / (Math.pow(1 + actualRate, n) - 1));
+        (Math.pow(1 + actualRate,1) / (Math.pow(1 + actualRate,1) - 1));
 
       setEmi(Math.round(calcemi));
-      setAmount(Math.round(calcemi * n));
-      setTotalinterest(Math.round(calcemi * n - p));
+      setAmount(Math.round(calcemi * 1));
+      setTotalinterest(Math.round(calcemi * 1 - p));
     } else {
       toast.error("Amount, Interest and Period must be greater than 0!", {
         position: "top-right",
@@ -49,6 +51,7 @@ const Calculator = () => {
   };
   return (
     <div className="container max-w-[1240px] m-auto py-20">
+        <ToastContainer />
       <div className="p-8 bg-white/10 z-10 backdrop-filter backdrop-blur-lg shadow-xl rounded-md ring-1 ring-gray-50">
         <h2 className="text-center py-5">Tax Calculator</h2>
         <div className="grid md:grid-cols-2 gap-10">
@@ -62,7 +65,7 @@ const Calculator = () => {
               <select
                 id="countries"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                <option defaultValue>purchase price</option>
+                <option defaultValue >purchase price</option>
                 <option value="balance">Balance</option>
               </select>
             </div>
@@ -71,6 +74,8 @@ const Calculator = () => {
               <input
                 type="text"
                 defaultValue={'25,00'}
+                value={principal}
+                onChange={handlePrincipalchange}
                 id="floating_outlined"
                 className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
@@ -89,8 +94,10 @@ const Calculator = () => {
               </label>
               <select
                 id="countries"
+                value={interest}
+                onChange={handleInterestchange}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                <option defaultValue>pay for goods or services</option>
+                <option defaultValue value={2.89}>pay for goods or services</option>
                 <option value="">Payment to friends and family</option>
                 <option value="">to collect donations</option>
                 <option value="">micropayment</option>
@@ -103,6 +110,7 @@ const Calculator = () => {
             </div>
             <div
               type="button"
+              onClick={calculateLoan}
               className="bg-blue-600 text-white text-center py-2 px-3 hover:bg-blue-500 rounded-md">
               Calculate
             </div>
@@ -112,7 +120,7 @@ const Calculator = () => {
               Calculation
             </h3>
             <div className="bg-gray-50 rounded-lg shadow-md p-5">
-              Paypal fees: <span className="ml-10">{interest}</span>
+              Paypal fees: <span className="ml-10">$ {totalinterest}</span>
             </div>
             <div className="bg-gray-50 rounded-lg shadow-md p-5">
               purchase price: <span className="ml-10">{amount}</span>
