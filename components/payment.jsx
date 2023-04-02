9cf110ad6c34df6c1813b45b157008abcd2ebf2f
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-04-02 23:59:30 +0600

    Update payment.jsx

--- components/payment.jsx
+++ components/payment.jsx
@@ -16,46 +16,46 @@ function Payment() {
       <table className='table-auto text-md sm:text-lg'>
            <thead>
             <tr className='border-t border-gray-200'>
-              <th class="px-5 py-5">Art der Zahlung</th>
-               <th class="px-5 py-5">Gebühren</th>
+              <th className="px-5 py-5">Art der Zahlung</th>
+               <th className="px-5 py-5">Gebühren</th>
               </tr>
         </thead>
            <tbody>
         <tr className='border-t border-gray-200'>
-        <td class=" px-5 py-5">Geld an Freunde und Familie senden</td>
-           <td class="px-5 py-5">kostenlos</td>
+        <td className=" px-5 py-5">Geld an Freunde und Familie senden</td>
+           <td className="px-5 py-5">kostenlos</td>
          </tr>
     <tr className='border-t border-gray-200'>
-      <td class=" px-5 py-5">Waren oder Dienstleistungen bezahlen</td>
-      <td class=" px-5 py-5">2,49% + 0,35€</td>
+      <td className=" px-5 py-5">Waren oder Dienstleistungen bezahlen</td>
+      <td className=" px-5 py-5">2,49% + 0,35€</td>
     </tr>
     <tr className='border-t border-gray-200'>
-      <td class=" px-5 py-5">Spenden sammeln</td>
-      <td class=" px-5 py-5">1,50% + 0,35€</td>
+      <td className=" px-5 py-5">Spenden sammeln</td>
+      <td className=" px-5 py-5">1,50% + 0,35€</td>
     </tr>
     <tr className='border-t border-gray-200'>
-      <td class=" px-5 py-5">Mikrozahlung</td>
-      <td class=" px-5 py-5">10% + 0,10€</td>
+      <td className=" px-5 py-5">Mikrozahlung</td>
+      <td className=" px-5 py-5">10% + 0,10€</td>
     </tr>
     <tr className='border-t border-gray-200'>
-      <td class=" px-5 py-5">Zahlung mit Händlerkonditionen (0 – 2,000€)</td>
-      <td class=" px-5 py-5">2,49% + 0,35€</td>
+      <td className=" px-5 py-5">Zahlung mit Händlerkonditionen (0 – 2,000€)</td>
+      <td className=" px-5 py-5">2,49% + 0,35€</td>
     </tr>
     <tr className='border-t border-gray-200'>
-      <td class=" px-5 py-5">Zahlung mit Händlerkonditionen (2.000,01 – 5.000€)</td>
-      <td class=" px-5 py-5">2,19% + 0,35€</td>
+      <td className=" px-5 py-5">Zahlung mit Händlerkonditionen (2.000,01 – 5.000€)</td>
+      <td className=" px-5 py-5">2,19% + 0,35€</td>
     </tr>
     <tr className='border-t border-gray-200'>
-      <td class=" px-5 py-5">Zahlung mit Händlerkonditionen (5.000,01 – 25.000€)</td>
-      <td class=" px-5 py-5">1,99% + 0,35€</td>
+      <td className=" px-5 py-5">Zahlung mit Händlerkonditionen (5.000,01 – 25.000€)</td>
+      <td className=" px-5 py-5">1,99% + 0,35€</td>
     </tr>
     <tr className='border-t border-gray-200'>
-      <td class=" px-5 py-5">Zahlung mit Händlerkonditionen (25.000,01 – 100.000€)</td>
-      <td class=" px-5 py-5">1,79% + 0,35€</td>
+      <td className=" px-5 py-5">Zahlung mit Händlerkonditionen (25.000,01 – 100.000€)</td>
+      <td className=" px-5 py-5">1,79% + 0,35€</td>
     </tr>
     <tr className='border-t border-b border-gray-200'>
-      <td class=" px-5 py-5">Zahlung mit Händlerkonditionen (mehr als 100.000€)</td>
-      <td class=" px-5 py-5">1,49% + 0,35€</td>
+      <td className=" px-5 py-5">Zahlung mit Händlerkonditionen (mehr als 100.000€)</td>
+      <td className=" px-5 py-5">1,49% + 0,35€</td>
     </tr>
   </tbody>
 </table>
